34f4196658591120237e4eaa5d257b8d984c11bd
MathiasSLarsen <mathias.s.larsen99@gmail.com>
2019-10-21 19:41:01 +0200

    Add files via upload

---
+++ MadRouletteAdmin/MadRouletteAdmin/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  MadRouletteAdmin
+//
+//  Created by Mathias Larsen on 21/10/2019.
+//  Copyright © 2019 MadneZ. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ MadRouletteAdmin/MadRouletteAdmin/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  MadRouletteAdmin
+//
+//  Created by Mathias Larsen on 21/10/2019.
+//  Copyright © 2019 MadneZ. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
